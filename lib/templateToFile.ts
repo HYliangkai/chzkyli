2b567d059c02419a082906aa4188ecb3d11ff0e8
chzky <hylk453030@gmail.com>
2024-03-20 11:22:51 +0800

    feat(html): add html template

--- lib/templateToFile.ts
+++ lib/templateToFile.ts
@@ -1,20 +1,9 @@
-import {
-  color,
-  dir,
-  Err,
-  fs,
-  Ok,
-  path,
-  prompt,
-  Result,
-  RunningError,
-} from './mod.ts'
+import {color, dir, Err, fs, Ok, path, prompt, Result, RunningError} from './mod.ts'
 
-const { __dirname } = dir.default(import.meta)
+const {__dirname} = dir.default(import.meta)
 const input_project_name = async () => {
   let project_name: string
-  loop_name:
-  while (true) {
+  loop_name: while (true) {
     project_name = await prompt.Input.prompt({
       message: `🚀 input your Project name:`,
       maxLength: 15,
@@ -36,23 +25,23 @@ const input_project_name = async () => {
   return project_name
 }
 
-type TemplateType = 'deno' | 'vue2-vite'
+type TemplateType = 'deno' | 'vue2-vite' | 'html'
 export const create_template_to_dir = async (
   template_dir_name: TemplateType,
+  copy_finish_callback?: (path: string, name: string) => Promise<void> | void
 ): Promise<Result<boolean, RunningError>> => {
   const project_name = await input_project_name()
-  const res = await fs.copy(
-    path.join(__dirname, `../static/template/${template_dir_name}`),
-    `./${project_name}`,
-  ).then(() => Ok(true)).catch(() =>
-    Err(new RunningError('❌ 项目创建失败,请重试'))
-  )
+  const res = await fs
+    .copy(path.join(__dirname, `../static/template/${template_dir_name}`), `./${project_name}`)
+    .then(() => Ok(true))
+    .catch(() => Err(new RunningError('❌ 项目创建失败,请重试')))
   if (res.is_err) return res
+
+  if (copy_finish_callback) await copy_finish_callback(`./${project_name}`, project_name)
+
   const p = Deno.run({
     cmd: ['code', `./${project_name}`],
   })
   const pres = await p.status()
-  return pres.success
-    ? Ok(true)
-    : Err(new RunningError('本地未找到code命令,请手动打开项目'))
+  return pres.success ? Ok(true) : Err(new RunningError('本地未找到code命令,请手动打开项目'))
 }
--- src/template/mod.ts
+++ src/template/mod.ts
@@ -1,29 +1,22 @@
-import {
-  cli,
-  color,
-  create_template_to_dir,
-  is_running_error,
-} from '../../lib/mod.ts'
+import {cli, color, create_template_to_dir, is_running_error} from '../../lib/mod.ts'
 
-const deno = new cli.Command()
-  .description(color.bgCyan('deno 项目模板'))
-  .action(async () => {
-    const res = await create_template_to_dir('deno')
-    res.match_err((e) => {
-      if (is_running_error(e)) {
-        console.log(e.message)
-      } else {
-        throw e
-      }
-    })
-    console.log(color.green('操作完成✅'))
+const deno = new cli.Command().description(color.bgCyan('deno 项目模板')).action(async () => {
+  const res = await create_template_to_dir('deno')
+  res.match_err(e => {
+    if (is_running_error(e)) {
+      console.log(e.message)
+    } else {
+      throw e
+    }
   })
+  console.log(color.green('操作完成✅'))
+})
 
 const vue2_vite = new cli.Command()
   .description(color.bgGreen('vue2.7-vite 项目模板'))
   .action(async () => {
     const res = await create_template_to_dir('vue2-vite')
-    res.match_err((e) => {
+    res.match_err(e => {
       if (is_running_error(e)) {
         console.log(e.message)
       } else {
@@ -33,7 +26,33 @@ const vue2_vite = new cli.Command()
     console.log(color.green('操作完成✅'))
   })
 
+const html = new cli.Command().description(color.bgRed('html基础项目模板')).action(async () => {
+  const rename_template = async (path: string, proj_name: string) => {
+    const decoder = new TextDecoder('utf-8')
+    await Promise.all([
+      Deno.writeTextFile(
+        `${path}/package.json`,
+        decoder.decode(await Deno.readFile(`${path}/package.json`)).replace('ZWF', proj_name)
+      ),
+      Deno.writeTextFile(
+        `${path}/index.html`,
+        decoder.decode(await Deno.readFile(`${path}/index.html`)).replace('ZWF', proj_name)
+      ),
+    ])
+  }
+  const res = await create_template_to_dir('html', rename_template)
+  res.match_err(e => {
+    if (is_running_error(e)) {
+      console.log(e.message)
+    } else {
+      throw e
+    }
+  })
+  console.log(color.green('操作完成✅'))
+})
+
 export default new cli.Command()
   .description('各项目模板')
   .command('deno', deno)
   .command('vue2', vue2_vite)
+  .command('html', html)
